f1660d56e053dc0313a1d3d3e17002a1017cbf3f
laurajimenez824 <laurajimenezanacona012@gmail.com>
2026-07-17 13:39:36 -0500

    Add files via upload

---
+++ Fase 5_problema 3.py
@@ -0,0 +1,20 @@
+Python 3.14.5 (tags/v3.14.5:5607950, May 10 2026, 10:43:50) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+============ RESTART: C:/Users/ACER-024/Downloads/fase5_problema3.py ===========
+INFORME DE PEDIDOS
+------------------------------
+Artículo: Teclado - Cantidad a pedir: 7
+Artículo: Mouse - Cantidad a pedir: 0
+Artículo: Monitor - Cantidad a pedir: 5
+Artículo: Impresora - Cantidad a pedir: 4
+Artículo: Memoria USB - Cantidad a pedir: 0
+>>> 
+============ RESTART: C:/Users/ACER-024/Downloads/fase5_problema3.py ===========
+INFORME DE PEDIDOS
+------------------------------
+Artículo: Teclado - Cantidad a pedir: 7
+Artículo: Mouse - Cantidad a pedir: 0
+Artículo: Monitor - Cantidad a pedir: 5
+Artículo: Impresora - Cantidad a pedir: 4
+Artículo: Memoria USB - Cantidad a pedir: 0
